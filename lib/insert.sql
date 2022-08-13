a96665e6ff6079189810a7e13c0a812dfa77d098
Justin Jeffries <justincyjeffries@gmail.com>
2022-08-12 22:53:42 -0400

    commit tree 2171efabdf8cf5056c68f0a934a0dedaa789cd7f

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'brown', 'mean', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 8, 'F', 'gray', 'drunk', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 2, 'F', 'spotted', 'coniving', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 13, 'M', 'brown', 'militant', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 1, 'F', 'white', 'forgiving', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 19, 'M', 'green', 'vengeful', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 48, 'F', 'bald', 'senile', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 13, 'M', 'mauve', 'conspiracy theorist', 1);
